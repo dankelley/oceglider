ad1f7a72edbc048b09b77faed029b536cb3b05c6
Clark Richards <clark.richards@gmail.com>
2019-04-01 08:08:28 -0300

    lonlatbug

--- sandbox/issue17/lonlatbug.R
+++ sandbox/issue17/lonlatbug.R
@@ -1,9 +1,14 @@
 library(oce)
 library(oceanglider)
 
-dir  <- "../data/sea019/m54/all_data/"
 dir  <- "/data/glider/2019/seaexplorer/sea019/m54/raw/"
 g <- read.glider.seaexplorer.raw(dir)
+
+head(g@data$payload[g@data$payload$yoNumber==886,])
+system(paste0("head ", dir, "sea019.54.pld1.raw.886"))
+
+head(g@data$payload[g@data$payload$yoNumber==887,])
+system(paste0("head ", dir, "sea019.54.pld1.raw.887"))
 
 head(g@data$payload[g@data$payload$yoNumber==888,])
 system(paste0("head ", dir, "sea019.54.pld1.raw.888"))
