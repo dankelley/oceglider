c3ffbb767477b761fb047380f9bc43fef03cc592
dankelley <kelley.dan@gmail.com>
2019-04-02 17:08:30 -0300

    create data

--- create_data/copyfiles.sh
+++ create_data/copyfiles.sh
@@ -1,9 +1,11 @@
+dir=/Users/kelley/git/glider/data
 ## realtime ("sub") data.
-cp -f /data/glider/2019/seaexplorer/sea019/m54/sub/sea019.54.gli.sub.[1-5].gz ../inst/extdata/seaexplorer/sub
-cp -f /data/glider/2019/seaexplorer/sea019/m54/sub/sea019.54.pld1.sub.[1-5].gz ../inst/extdata/seaexplorer/sub
+cp -f ${dir}/sea019/m54/nav/logs/sea019.54.gli.sub.10[1-6].gz ../inst/extdata/seaexplorer/sub
+cp -f ${dir}/sea019/m54/nav/logs/sea019.54.pld1.sub.10[1-6].gz ../inst/extdata/seaexplorer/sub
 ## delayed ("raw") data
-cp -f /data/glider/2019/seaexplorer/sea019/m49/raw/sea019.54.gli.sub.[1-5].gz ../inst/extdata/seaexplorer/raw
-cp -f /data/glider/2019/seaexplorer/sea019/m49/raw/sea019.54.pld1.sub.[1-5].gz ../inst/extdata/seaexplorer/raw
+cp -f ${dir}/sea019/m54/all_data/sea019.54.gli.sub.10[1-6] ../inst/extdata/seaexplorer/raw
+cp -f ${dir}/sea019/m54/all_data/sea019.54.pld1.raw.10[1-6] ../inst/extdata/seaexplorer/raw
+gzip ../inst/extdata/seaexplorer/raw/*
 ## fix permissions, just in case
 chmod -x ../inst/extdata/seaexplorer/sub/*
 chmod -x ../inst/extdata/seaexplorer/raw/*
