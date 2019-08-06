698c6f8fec2c29d1b900d2d356f36c928ffbe64e
dankelley <kelley.dan@gmail.com>
2019-08-06 19:00:20 -0300

    keypress

--- sandbox/dk/qc/notes/test_keypress.R
+++ sandbox/dk/qc/notes/test_keypress.R
@@ -4,11 +4,17 @@ runApp( list(ui = bootstrapPage(
   tags$script('
     $(document).on("keypress", function (e) {
        Shiny.onInputChange("mydata", e.which);
-    });
+       Shiny.onInputChange("keypressTrigger", Math.random());
+});
   ') 
 ) , server = function(input, output, session) {
-  output$results = renderPrint({
-    input$mydata
-  })
-}
-))
+      output$results = renderText({
+          if (!is.null(input$mydata)) {
+              key <- intToUtf8(input$mydata)
+              if (key == 'q') stop("experiment complete") else key
+          } else {
+              "click window and then type letters ('q' to quit)."
+          }
+      })
+  }
+  ))
