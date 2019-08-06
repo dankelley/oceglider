060b67538d48f4a419b1dcb0a296e0aa0ee2a097
dankelley <kelley.dan@gmail.com>
2019-08-06 19:11:31 -0300

    report keypress as both number and char

--- sandbox/dk/qc/notes/test_keypress.R
+++ sandbox/dk/qc/notes/test_keypress.R
@@ -11,7 +11,7 @@ runApp( list(ui = bootstrapPage(
       output$results = renderText({
           if (!is.null(input$mydata)) {
               key <- intToUtf8(input$mydata)
-              if (key == 'q') stop("experiment complete") else key
+              if (key == 'q') stop("experiment complete") else paste("numeric=", input$mydata, " character='", key, "'", sep="")
           } else {
               "click window and then type letters ('q' to quit)."
           }
