e7e7f0d354ed0f89ba367e08bd46f72f3feb0a04
dankelley <kelley.dan@gmail.com>
2019-08-08 14:06:33 -0300

    N2 test

--- sandbox/dk/qc/dev/20190808_N2/N2_test.R
+++ sandbox/dk/qc/dev/20190808_N2/N2_test.R
@@ -1,50 +1,74 @@
 library(oceanglider)
 options(oceEOS="gsw") # A *lot* of code is hard-wired for this, so we don't let user choose.
 
-N2profile <- function(pressure, sigma0, g=9.8, L=2, rho0=1025)
+N2unstable <- function(pressure, sigma0, g=9.8, L=2, rho0=1025)
 {
     z <- swZ(pressure)
     o <- order(z)
     oo <- order(o)
     if (L > 0) {
         m <- runlm(z[o], sigma0[o], L=L)
-        rval <- -g / rho0 * m$dydx
+        N2 <- -g / rho0 * m$dydx
     } else {
-        rval <- -g / rho0 * diff(sigma0)/diff(z)
-        rval <- c(rval[1], rval)
+        N2 <- -g / rho0 * diff(sigma0)/diff(z)
+        N2 <- c(rval[1], rval)
     }
-    rval[!is.finite(rval)] <- NA
-    rval[oo]
+    N2[!is.finite(N2)] <- NA
+    N2 <- N2[oo] # return to original order
+    unstable <- N2 < (-mean(N2[N2>0], na.rm=TRUE))
+    list(N2=N2, unstable=unstable)
 }
 
+file <- "../../sea021m51_unedited.rda"
+
 if (!exists("g")) {
-    load("../../sea021m51_unedited.rda")
+    load(file)
 }
 
 max <- max(g[["yoNumber"]])
 
-pdf("N2_test.pdf")
+createPDF <- TRUE
+
+if (createPDF) pdf("N2_test.pdf")
 
 par(mfrow=c(1, 2), mar=c(3, 3, 1.5, 1), mgp=c(2, 0.7, 0))
 for (y in 1:max) {
+#for (y in 350) {
     message("y=", y)
     yo <- subset(g, yoNumber==y)
     sigma0 <- yo[["sigma0"]]
     p <- yo[["pressure"]]
-    o <- order(p)
-    sigma0 <- sigma0[o]
-    p <- p[o]
-    bad <- is.na(p) | is.na(sigma0) | p > 300
-    p <- p[!bad]
-    sigma0 <- sigma0[!bad]
-    cex <- 0.5
-    pch <- 20
-    plot(sigma0, p, ylim=rev(range(p)), pch=pch, cex=cex)
-    N2 <- N2profile(p, sigma0)
-    unstable <- N2 < (-mean(N2[N2>0]))
-    plot(N2, p, ylim=rev(range(p)), pch=pch, cex=cex, col=ifelse(unstable, "red", "gray"))
-    mtext(sprintf("yo=%d", y), side=3)
+    ##o <- order(p)
+    ##sigma0 <- sigma0[o]
+    ##p <- p[o]
+    ##bad <- is.na(p) | is.na(sigma0) | p > 300
+    ##> bad <- is.na(p) | p > 300
+    ##> p <- p[!bad]
+    ##> sigma0 <- sigma0[!bad]
+    N2u <- N2unstable(p, sigma0)
+    N2 <- N2u$N2
+    unstable <- N2u$unstable
+    col <- ifelse(unstable, "red", "gray")
+    cex <- ifelse(unstable, 1, 0.5)
+    pch <- ifelse(unstable, 20, 20)
+    if (any(is.finite(p)) && any(is.finite(sigma0)) && any(is.finite(N2))) {
+        ylim <- rev(range(p, na.rm=TRUE))
+        plot(sigma0, p, ylim=ylim, pch=pch, cex=cex, col=col, xlab=expression(sigma[0]))
+        mtext(file, side=3)
+        plot(N2, p, ylim=ylim, pch=pch, cex=cex, col=col, xlab=expression(N^2))
+        abline(v=0, col='pink')
+        mtext(paste("yo", y), side=3)
+    } else {
+        plot(0:1, 0:1, xlab="", ylab="", type="n", axes=FALSE)
+        text(0.5, 0.5, "No valid (p, sigma0, N2) data")
+        box()
+        mtext(file, side=3)
+        plot(0:1, 0:1, xlab="", ylab="", type="n", axes=FALSE)
+        text(0.5, 0.5, "No valid (p, sigma0, N2) data")
+        box()
+        mtext(file, side=3)
+    }
 }
 
-dev.off()
+if (createPDF) dev.off()
 
